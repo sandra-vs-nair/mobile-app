da12526fce1546db9a8ad96045dc362744f0af66
sandra <sandravsnair@gmail.com>
2020-09-11 12:17:17 +0530

    Mobile application

---
+++ README.md
@@ -0,0 +1,89 @@
+# Mobile application using python.
+
+This project demonstrates a mobile application. It has
+1. Login feature.
+2. Signup feature.
+3. Receives user input on how he is feeling at the moment. (happy,sad or unloved)
+4. Displays a quote according to his mood.
+
+## Prerequisites
+
+Install kivy library for designing the application using the below commands.
+
+Mac and Linux users:
+
+Kivy currently only works with Python 3.7 or earlier on Mac and Linux.
+
+````
+
+python3.7 -m pip install kivy
+
+````
+
+Windows users:
+
+If you are using Python 3.7 or earlier, run all three following commands one by one:
+
+````
+
+pip install kivy
+
+````
+
+````
+
+pip install kivy.deps.glew
+
+````
+
+````
+
+pip install docutils pygments pypiwin32 kivy.deps.sdl2
+
+````
+
+If you are using Python 3.8, run all four following commands one by one:
+
+````
+
+pip install --upgrade pip setuptools wheel
+
+````
+
+````
+
+pip install https://github.com/kivy/kivy/archive/master.zip
+
+````
+
+````
+
+pip install kivy.deps.glew
+
+````
+
+````
+
+pip install docutils pygments pypiwin32 kivy.deps.sdl2
+
+````
+
+## Usage
+
+Run the python file main.py. Use the commands below in any terminal.
+
+````
+python main.py
+
+````
+or if the python version is 3, use command.
+
+````
+python3 main.py
+
+````
+or you can also use any python IDE.
+
+## Creating APK for android
+
+You can use buildozer tool and create an android apk for the application.
---
+++ main.py
@@ -0,0 +1,85 @@
+# -----------------------------------------------------------
+# Mobile Application logic
+#
+# (C) 2020 Sandra VS Nair, Trivandrum
+# email sandravsnair@gmail.com
+# -----------------------------------------------------------
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager,Screen
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+
+from json.decoder import JSONDecodeError
+from datetime import datetime
+import json, glob, random
+from pathlib import Path
+
+#For button-hover feature.
+from hoverable import HoverBehavior
+
+#Loading the kivy file.
+Builder.load_file("design.kv")
+
+class LoginScreen(Screen):
+    def sign_up(self):
+        self.manager.current="sign_up_screen"
+    def login(self,uname,pwd):
+        #FIXME
+        #json.load will fail if users.json is empty.
+        with open("users.json") as file:
+            users=json.load(file)
+            if uname in users and users[uname]['password']==pwd:
+                self.manager.current="login_success"
+            else:
+                self.ids.login_wrong.text='Invalid username or password'
+                    
+        
+class LoginSuccessScreen(Screen):
+    def logout(self):
+        self.manager.transition.direction='right'
+        self.manager.current="login_screen"
+    def display_quote(self,feel):
+        feel=feel.lower()
+        available_feelings=glob.glob("*txt")
+        
+        available_feelings=[Path(filename).stem for filename in available_feelings]
+        print(available_feelings)
+        
+        if feel in available_feelings:
+            with open(f"{feel}.txt",encoding="utf8") as file:
+                quotes=file.readlines()
+            self.ids.quote.text=random.choice(quotes)
+        else:
+            self.ids.quote.text="Try another feeling"
+                
+        
+class SignupScreen(Screen):
+    def add_user(self,uname,pwd):
+        #FIXME
+        #json.load will fail if users.json is empty.
+        with open("users.json",'r+') as file:
+            users=json.load(file)
+            users[uname]={'username':uname,'password':pwd,
+                             'created':datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+            json.dump(users, file)   
+        self.manager.current="sign_up_success"
+        
+class SignupSuccessScreen(Screen):
+    def login_page(self):
+        self.manager.transition.direction='right'
+        self.manager.current="login_screen"
+
+class ImageButton(ButtonBehavior,HoverBehavior,Image):
+    pass
+    
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+    
+if __name__=="__main__":
+    MainApp().run()
